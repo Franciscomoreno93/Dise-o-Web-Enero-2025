4016ceab2da4e8db91608697fadad16dbc93e888
Franciscomoreno93 <kirumoreno93@alumnos.cei.es>
2025-03-15 21:11:25 +0100

    commit88

--- ligthbox/index.js
+++ ligthbox/index.js
@@ -1,15 +1,15 @@
 const imgList = document.querySelectorAll(`.img`);
-const lightbox = document.querySelector(`.lightbox`);
-const grande = document.querySelector(`.grande`);
-const closeBtn = document.querySelector(`.close`);
+const lightbox = document.querySelector(`.lightbox`); // Obtiene la referencia al elemento ligthbox
+const grande = document.querySelector(`.grande`); // Obtiene la referencia a la imagen ampliada
+const closeBtn = document.querySelector(`.close`); // Obtiene la referencia al botón de cierre
 
 imgList.forEach(( eachImg , index )=>{
-    imgList[index].addEventListener(`click`,()=>{
-        lightbox.classList.add(`isActive`);
+    imgList[index].addEventListener(`click`,()=>{ // Añade un evento de clic a cada imagen
+        lightbox.classList.add(`isActive`); // Activa el ligthbox añadiendo la clase "isActive"
         grande.src = imgList[index].src
     })
 })
 
-closeBtn.addEventListener(`click`,()=>{
+closeBtn.addEventListener(`click`,()=>{ // Añade un evento de clic al botón de cierre
     lightbox.classList.remove(`isActive`)
 })
