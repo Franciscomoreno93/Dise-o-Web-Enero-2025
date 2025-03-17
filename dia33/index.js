e1e8dd2a4015556a5442ebe98356fb3a04e8bbc4
Franciscomoreno93 <kirumoreno93@alumnos.cei.es>
2025-03-17 11:38:04 +0100

    commit91

--- dia33/index.js
+++ dia33/index.js
@@ -1,7 +1,3 @@
-// Operadores
-console.log("1 < 2");
-console.log(1 < 2);
-
 // Ejercicio 1
 const p1 = document.querySelector("#p1");
 const bt1 = document.querySelector("bt1");
@@ -19,3 +15,19 @@ function handleEjercicio1(e) {
 }
 
 bt1.addEventListener("click", handleEjercicio1)
+
+// Ejercicio 2
+const box2 = document.querySelector(".box2");
+const bt2 = document.querySelector("#bt2");
+
+function handleEjercicio2(e) {
+    if(box2.style.width == "80vw"){
+        box2.style.width = "20vw";
+    }else{
+        box2.style.width = "80vw";
+    }
+}
+
+bt2.addEventListener("click", handleEjercicio2)
+
+// Ejercicio 3
